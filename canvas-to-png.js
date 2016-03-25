9332bca7358b8eca8b59ad6f4bdee45bde089d0c
Roben Kleene <robenk@gmail.com>
2016-03-24 21:23:22 -0400

    Start drawing grid

--- canvas-to-png.js
+++ canvas-to-png.js
@@ -1,9 +1,43 @@
 window.onload = function() {
-  var canvas = document.getElementById("output");
+  var canvas = document.getElementById("input");
   var context = canvas.getContext("2d");
-  context.fillStyle = "green";
-  context.fillRect(50, 50, 100, 100);
-  // no argument defaults to image/png; image/jpeg, etc also work on some
-  // implementations -- image/png is the only one that must be supported per spec.
-  // window.location = canvas.toDataURL("image/png");
-}
+
+  var gridSize = {
+    width: 6,
+    height: 3
+  };
+
+  var cellSize = {
+    width: canvas.width / gridSize.width,
+    height: canvas.height / gridSize.height
+  };
+
+  // Background
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+
+
+  // Grid Lines
+  for (var i = 0; i < gridSize.width; i++) {
+    // context.rect(x, y, cellSize.width, cellSize.height);
+    // context.stroke();
+    for (var j = 0; j < gridSize.height; j++) {
+      var x = i * cellSize.width;
+      var y = j * cellSize.height;
+      context.rect(x, y, cellSize.width, cellSize.height);
+      context.stroke();
+    }
+  }
+
+
+
+  var img = new Image(canvas.width, canvas.height);
+
+
+  var dataURL = canvas.toDataURL("image/jpeg");
+  img.src = dataURL;
+  document.getElementById('output').appendChild(img);
+
+  // document.getElementById('output').src = dataURL;
+};
